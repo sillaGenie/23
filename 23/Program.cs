2c46cbfbf4ca57cde986a4b6124797f37f98a4b7
ADM <ADM@A320M-H-CF>
2022-02-06 19:01:51 +0300

    Добавьте файлы проекта.

---
+++ 23/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object n = Convert.ToInt32(Console.ReadLine());
+            int r = FactorialAsync(n).Result;
+            Console.WriteLine(r);
+        }
+        static  int Factorial(object n)
+        {
+            int x=(int)n;
+            int c = 1;
+            while (x>1)
+            {
+                c *= x--;
+            }
+            return c;
+
+        }
+        static async Task<int> FactorialAsync(object n)
+        {
+            int result = await Task.Run(() => Factorial(n));
+            return result;
+        }
+    }
+}
+
